d7d1ab6bdab0d43cdf7e4b748dd6b2ecf5e46325
Peaklabdevelopers <peaklabdevelopers>
2020-06-05 19:58:15 -0400

    wasabipapucho

---
+++ models/libros99.js
@@ -0,0 +1,27 @@
+const mong9 = require("mongoose");
+const Schema = mong9.Schema;
+
+const lir = new Schema({
+  title: {
+    type: String,
+  },
+
+  authors: {
+    type: String,
+  },
+
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+});
+
+
+const documents = mongoose.model("documents", lir);
+
+module.exports = documents;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mong9 = require("moongose");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks_db",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
